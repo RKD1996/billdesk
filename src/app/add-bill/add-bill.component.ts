a2dcd095816ef0e5f30191829aedc246fe4a162d
rishi90kesh <rishikesh.singh@ia.ooo>
2019-11-11 18:37:57 +0530

    asdasd

--- src/app/add-bill/add-bill.component.ts
+++ src/app/add-bill/add-bill.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 
 @Component({
@@ -7,13 +8,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-bill.component.scss']
 })
 export class AddBillComponent implements OnInit {
-
-  constructor() {
+  exp = {
+    name: String = '',
+    amt: Number,
+    date: Date
+  }
+  final_data: any = [];
+  constructor(private netcall: HttpClient) {
 
   }
 
   ngOnInit() {
 
+  }
+
+  add() {
+    this.final_data.push(this.exp);
+    this.exp = {
+      name: '',
+      amt: Number,
+      date: Date
+    }
+    console.log(this.final_data);
+  }
+
+  save() {
+      this.netcall.post('http://localhost:3000/bils/create_exp', {exp: this.final_data}).subscribe(res => {
+        console.log(res)
+      });
   }
 
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,26 +8,22 @@ import { HttpClient } from '@angular/common/http';
 })
 export class HomeComponent implements OnInit {
 
-  bill_data: any = []
-  latest_bill: any = []
+
+  bill_data: any
+  latest_bill: any;
   total;
 
 
-  constructor(private netcall: HttpClient) { }
+  constructor(private netcall: HttpClient) {
+
+   }
 
   ngOnInit() {
-    this.getData()
-  }
+    this.netcall.get("http://localhost:3000/bils/show").subscribe( res => {
 
-  getData() {
-    this.netcall.get("http://localhost:3000/bils/show").subscribe(function (res) {
       this.bill_data = res.bills;
       this.latest_bill = res.latest;
       this.total = res.total;
-      console.log(this.bill_data);
-      console.log(this.latest_bill);
-      console.log(this.total);
     });
   }
-
 }
